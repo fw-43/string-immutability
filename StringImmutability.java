5710fb4234d05fc10c029613bdeb089f2a409d8c
fw <fauziwahas@proton.me>
2026-07-26 13:34:57 +0700

    update

--- StringImmutability.java
+++ StringImmutability.java
@@ -4,6 +4,13 @@ public class StringImmutability {
 
   public StringImmutability() {
     Scanner scanner = new Scanner(System.in);
+    String string = "a";  // +1
+    string = "aa";        // +1
+    string = "aaa";       // +1
+    string = "a";         // +0 (existing string)
+    string = "aa";        // +0 (existing string)
+    string = "bb";        // +1
+    System.out.println(string);
     System.out.print("Press any key to exit: ");
     scanner.nextLine();
     scanner.close();
